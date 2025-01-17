b9c59adcef5f89d44873d829ab38628e2c0a2631
Fayel MOHAMED <fayel.mohamed@roborative.com>
2025-01-17 16:07:46 +0100

    métier

--- back-end/src/bin/test.rs
+++ back-end/src/bin/test.rs
@@ -1,91 +1,32 @@
-use quick_xml::events::Event;
-use quick_xml::Reader;
+use std::fs;
+extern crate serde;
+extern crate quick_xml;
+use quick_xml::{
+    de::{from_reader, from_str}, events::Event, Reader
+};
+use serde::{Deserialize, Serialize};
 
-
-#[derive(Debug)]
+#[derive(Debug, Deserialize, Serialize, PartialEq)]
 struct Metier {
-    nom_metier: String,
-    formats_courts: Vec<String>,
-    acces_metier: String,
+    pub identifiant: String,
+    pub nom_metier: String,
+    pub acces_metier: String
+}
+
+#[derive(Debug, Deserialize, Serialize, PartialEq)]
+struct Metiers {
+    #[serde(rename = "metier")]
+    pub metiers: Vec<Metier>,
 }
 // Onisep_Ideo_Fiches_Metiers_09122024.xml
 fn main() {
-    // Ouvrir le fichier XML
+    let file = fs::read_to_string("Onisep_Ideo_Fiches_Metiers_09122024.xml").expect("Cannot read");
+    let res:Metiers = from_str(&file).expect("Deserialize failed :");
 
-    // Créer un lecteur XML
-    let mut xml_reader = Reader::from_file("Onisep_Ideo_Fiches_Metiers_09122024.xml").unwrap();
-    xml_reader.config_mut().trim_text(true);
-
-    let mut buf = Vec::new();
-    let mut metier = Metier {
-        nom_metier: String::new(),
-        formats_courts: Vec::new(),
-        acces_metier: String::new(),
-    };
-
-    let mut in_nom_metier = false;
-    let mut in_formats_courts = false;
-    let mut in_acces_metier = false;
-    let mut in_format_court = false;
-
-    loop {
-        match xml_reader.read_event_into(&mut buf) {
-            Ok(Event::Start(ref e)) => {
-                match e.name().as_ref() {
-                    b"nom_metier" => {
-                        in_nom_metier = true;
-                    }
-                    b"formats_courts" => {
-                        in_formats_courts = true;
-                    }
-                    b"acces_metier" => {
-                        in_acces_metier = true;
-                    }
-                    b"format_court" => {
-                        if in_formats_courts {
-                            in_format_court = true;
-                        }
-                    }
-                    _ => {}
-                }
-            }
-            Ok(Event::Text(e)) => {
-                if in_nom_metier {
-                    metier.nom_metier = e.unescape().unwrap().to_string();
-                }
-                if in_acces_metier {
-                    metier.acces_metier = e.unescape().unwrap().to_string();
-                }
-                if in_format_court {
-                    metier.formats_courts.push(e.unescape().unwrap().to_string());
-                }
-            }
-            Ok(Event::End(ref e)) => {
-                match e.name().as_ref() {
-                    b"nom_metier" => {
-                        in_nom_metier = false;
-                    }
-                    b"formats_courts" => {
-                        in_formats_courts = false;
-                    }
-                    b"acces_metier" => {
-                        in_acces_metier = false;
-                    }
-                    b"format_court" => {
-                        if in_formats_courts {
-                            in_format_court = false;
-                        }
-                    }
-                    _ => {}
-                }
-            }
-            Ok(Event::Eof) => break, // Sortir de la boucle à la fin du fichier
-            Err(e) => panic!("Error at position {}: {:?}", xml_reader.buffer_position(), e),
-            _ => {}
+    for metier in res.metiers {
+        if metier.nom_metier.contains("développeur"){
+            println!("{:?}",metier);
         }
-        buf.clear();
     }
-
-    // Afficher les valeurs extraites
-    println!("{:?}", metier);
+    
 }

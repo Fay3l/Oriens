7535c430e07da6472d5fbf82911cba3f21ebca45
Fayel MOHAMED <fayel.mohamed@roborative.com>
2025-03-31 09:52:05 +0200

    linear gradient

--- front-end/src/components/button/OriensButton.vue
+++ front-end/src/components/button/OriensButton.vue
@@ -8,7 +8,14 @@ const props = defineProps({
 </script>
 
 <template>
-    <button class="bg-orange text-white font-bold py-5 px-5 rounded-lg shadow-md hover:bg-orangelight hover:shadow-lg transition duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-105">
+    <button
+        class="linegradient text-white py-5 px-5 rounded-lg shadow-md hover:bg-orangelight hover:shadow-lg transition duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-105">
         {{ props.label }}
     </button>
 </template>
+
+<style scoped lang="css">
+    .linegradient{
+        background: linear-gradient(to bottom , #EE7213 0%, #F09A4E 100%);
+    }
+</style>
